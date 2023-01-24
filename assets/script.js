dfcbfaf9ba56a4b9c3335705fd4dcbb6212fb421
Connor Stroh <connorstroh@Connors-MacBook-Pro.local>
2023-01-24 10:56:07 -0600

    JS Initial

---
+++ assets/script.js
@@ -0,0 +1,4 @@
+const cityModel = document.querySelector("#city-model");
+const cityInput = document.querySelector("#city-input");
+const cityNameEl = document.querySelector("#city-name");
+const currentDateEl = document.querySelector("#current-date");
